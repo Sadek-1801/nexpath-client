28e8baf4e755e81c6f3ea80354f49c686fe679a8
Sadek-1801 <hmashrafujjaman@gmail.com>
2024-05-15 23:51:46 +0600

    finishing just for functionality purpose

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import TabCard from "../components/TabCard";
 import Slider from "../components/Slider";
+import Newsletter from "../components/Newsletter";
+import BlogCard from "../components/BlogCard";
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
             <TabCard></TabCard>
+            <BlogCard></BlogCard>
+            <Newsletter />
+
         </div>
     );
 };
